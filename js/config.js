0d07b4e52d337f31250bcc4a4348cce0284376d5
juniorjfsn <43158906+juniorjfsn@users.noreply.github.com>
2018-12-13 12:16:37 -0300

    PPG-vers-0.11
    Aplicativo PPG

--- js/config.js
+++ js/config.js
@@ -3,14 +3,6 @@
 // var imei = '865592031966260';
 
 
-
-//========= device =============//
-var device;
-(function (device) {
-    function uuid() { }
-    device.uuid = uuid;
-})(device || (device = {}));
-//========= device =============//
-var device.uuid = '865592031966260';
+ 
 
 

8945a73830987cb61bf132d381f7b7edfd129ef5
juniorjfsn <43158906+juniorjfsn@users.noreply.github.com>
2018-12-13 09:39:15 -0300

    PPG-vers-0.07
    Aplicativo PPG

---
+++ js/device.js
@@ -0,0 +1,18 @@
+var device;
+(function (device) {
+    function uuid() { }
+    device.uuid = uuid;
+})(device || (device = {}));
+
+var gitbub ;
+
+/*
+namespace device {
+    export class uuid {
+        uuid : string;
+        constructor(uuid) {
+            this.uuid = uuid ;
+        }
+    }
+}
+*/ 
---
+++ js/device.ts
@@ -0,0 +1,18 @@
+namespace device { 
+	export function uuid() {} 
+}
+declare module "device" {
+    export = device;
+}
+
+var gitbub ;
+/*
+namespace device { 
+	export class uuid { 
+		uuid : string;
+		constructor(uuid) {
+			this.uuid = uuid ;
+		}
+	}
+}
+*/
